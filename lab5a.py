e3d41c18b841e2e3dbac24494614b137898d71c1
Sabin Bhujel <sbhujel2@myseneca.ca>
2024-07-12 17:11:00 -0400

    lab5a completed.

---
+++ lab5a.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# Author ID: sbhujel2
+
+def read_file_string(file_name):
+    # Takes file_name as string for a file name, returns its entire contents as a string
+    try:
+        with open(file_name, 'r') as file:
+            return file.read()
+    except FileNotFoundError:
+        return f"File {file_name} not found."
+    except Exception as e:
+        return str(e)
+
+def read_file_list(file_name):
+    # Takes a file_name as string for a file name, 
+    # returns its entire contents as a list of lines without new-line characters
+    try:
+        with open(file_name, 'r') as file:
+            return [line.strip() for line in file]
+    except FileNotFoundError:
+        return f"File {file_name} not found."
+    except Exception as e:
+        return str(e)
+
+if __name__ == '__main__':
+    file_name = 'data.txt'
+    print(read_file_string(file_name))
+    print(read_file_list(file_name))
+
